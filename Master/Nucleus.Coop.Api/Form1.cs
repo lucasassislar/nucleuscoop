5c20b8cc0d92d0c23b19dedb9c5d2933d5a4628a
r-mach <remimach.pro@gmail.com>
2018-05-15 21:27:45 +0200

    Adding RestSharp integration exemple with the current beta api.

---
+++ Master/Nucleus.Coop.Api/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Nucleus.Coop.Api
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var answer = ApiController.Register(textBoxRegUsername.Text, textBoxRegEmail.Text, textBoxRegPassword.Text);
+            richTextBox1.Text = answer;
+        }
+
+        private void buttonLogSend_Click(object sender, EventArgs e)
+        {
+            var answer = ApiController.Login(textBoxLogEmail.Text, textBoxLogPassword.Text);
+            richTextBox1.Text = answer;
+        }
+
+        private void buttonSearchSend_Click(object sender, EventArgs e)
+        {
+            var answer = ApiController.Search(textBoxSearchGameName.Text);
+            richTextBox1.Text = answer;
+        }
+
+        private void buttonSetToken_Click(object sender, EventArgs e)
+        {
+            ApiController.Token = textBoxToken.Text;
+            richTextBox1.Text = "Token set: " + ApiController.Token;
+        }
+    }
+}
