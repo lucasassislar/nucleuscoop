52e622be91a0290a956fe8ecd0ac2f52a6830a74
r-mach <remimach.pro@gmail.com>
2018-05-19 03:03:57 +0200

    More work on the API integration (client side), with useful demonstration. It needs to be wrapped correctly now (error handling, and so on...)...

--- Master/Nucleus.Coop.Api/Api/ApiController.cs
+++ Master/Nucleus.Coop.Api/Api/ApiController.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Text;
 using System.Windows.Forms;
 using RestSharp;
+using System.IO;
 
 namespace Nucleus.Coop.Api
 {
@@ -26,13 +27,10 @@ public class ApiController : UserControl
             request.RequestFormat = DataFormat.Json;
             request.AddJsonBody(new { username, password, email });
 
-            //We can map automagically Json answers to classes. Uncomment the two following comments to see the result.
-
-            //IRestResponse<User> response = client.Execute<User>(request);
             IRestResponse response = client.Execute(request);
             return response.Content;
-            //return response.Data.username;
         }
+
         public static String Login(string email, string password)
         {
             var request = new RestRequest("auth/login", Method.POST);
@@ -41,7 +39,8 @@ public class ApiController : UserControl
             IRestResponse response = client.Execute(request);
             return response.Content;
         }
-        public static String Search(string gameName)
+
+        public static String SearchExtGame(string gameName)
         {
             var request = new RestRequest("game/api/search/{gameName}", Method.GET);
             request.AddHeader("Authorization", "Bearer " + Token);
@@ -50,6 +49,70 @@ public class ApiController : UserControl
 
             IRestResponse response = client.Execute(request);
             return response.Content;
+        }
+
+
+        public static String ImportExtGame(string gameIGDBId)
+        {
+            var request = new RestRequest("game/api/import/{gameIGDBId}", Method.POST);
+            request.AddHeader("Authorization", "Bearer " + Token);
+            request.AddUrlSegment("gameIGDBId", gameIGDBId);
+            request.RequestFormat = DataFormat.Json;
+
+            IRestResponse response = client.Execute(request);
+            return response.Content;
+        }
+
+        public static IRestResponse<List<Game>> ListIntGames()
+        {
+            var request = new RestRequest("game", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+
+            IRestResponse<List<Game>> response = client.Execute<List<Game>>(request);
+            return response;
+        }
+
+        public static IRestResponse<Game> GetSpecificGameWithHandlers(int internalGameId)
+        {
+            var request = new RestRequest("game/{internalGameId}", Method.GET);
+            request.AddUrlSegment("internalGameId", internalGameId);
+            request.RequestFormat = DataFormat.Json;
+
+            IRestResponse<Game> response = client.Execute<Game>(request);
+            return response;
+        }
+
+
+        public static IRestResponse<Handler> CreateHandler(int gameId, string handlerName, string handlerDetails)
+        {
+            var request = new RestRequest("handler", Method.POST);
+            request.AddHeader("Authorization", "Bearer " + Token);
+            request.AddJsonBody(new { game_id = gameId, name = handlerName, details = handlerDetails });
+            request.RequestFormat = DataFormat.Json;
+            IRestResponse<Handler> response = client.Execute<Handler>(request);
+            return response;
+        }
+
+        public static IRestResponse<Package> CreatePackage(int handlerId, string packageFullPath, string packageInfos)
+        {
+            var request = new RestRequest("handler/{handlerId}", Method.POST);
+            request.AddHeader("Authorization", "Bearer " + Token);
+            request.AddUrlSegment("handlerId", handlerId);
+            request.AddFile("package", @packageFullPath);
+            request.AddParameter("infos", packageInfos);
+            IRestResponse<Package> response = client.Execute<Package>(request);
+            return response;
+        }
+
+        public static bool GetPackage(int handlerId, int specificVersion = -1)
+        {
+            var request = new RestRequest("handler/{handlerId}/{specificVersion}", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddUrlSegment("handlerId", handlerId);
+            request.AddUrlSegment("specificVersion", specificVersion >= 1 ? Convert.ToString(specificVersion) : "latest");
+            var fileBytes = client.DownloadData(request);
+            File.WriteAllBytes("package.nc", fileBytes);
+            return true; // As for now. Should be correctly wrapped (and return false or errorcode on error...)
         }
     }
 }
--- Master/Nucleus.Coop.Api/Form1.cs
+++ Master/Nucleus.Coop.Api/Form1.cs
@@ -32,7 +32,7 @@ namespace Nucleus.Coop.Api
 
         private void buttonSearchSend_Click(object sender, EventArgs e)
         {
-            var answer = ApiController.Search(textBoxSearchGameName.Text);
+            var answer = ApiController.SearchExtGame(textBoxSearchGameName.Text);
             richTextBox1.Text = answer;
         }
 
@@ -40,6 +40,55 @@ namespace Nucleus.Coop.Api
         {
             ApiController.Token = textBoxToken.Text;
             richTextBox1.Text = "Token set: " + ApiController.Token;
+        }
+
+        private void buttonImportSend_Click(object sender, EventArgs e)
+        {
+            var answer = ApiController.ImportExtGame(textBoxImportId.Text);
+            richTextBox1.Text = answer;
+        }
+
+        private void buttonListGameSend_Click(object sender, EventArgs e)
+        {
+            var answer = ApiController.ListIntGames();
+            richTextBox1.Text = answer.Content;
+            dataGridViewListGames.DataSource = answer.Data;
+        }
+
+        private void buttonSpecificGameSend_Click(object sender, EventArgs e)
+        {
+            var answer = ApiController.GetSpecificGameWithHandlers(Convert.ToInt32(textBoxSpecificGameId.Text));
+            //richTextBox1.Text = answer;
+            dataGridViewListHandlers.DataSource = answer.Data.handlers;
+        }
+
+        private void buttonAddHandlerSend_Click(object sender, EventArgs e)
+        {
+            var answer = ApiController.CreateHandler(Convert.ToInt32(textBoxAddHandlerGameId.Text), textBoxAddHandlerName.Text, textBoxAddHandlerDetails.Text);
+            richTextBox1.Text = answer.Content;
+        }
+
+        private void textBoxAddPackageFileFullPath_Click(object sender, EventArgs e)
+        {
+            openFileDialogAddPackageFileFullPath.ShowDialog();
+        }
+
+        private void openFileDialogAddPackageFileFullPath_FileOk(object sender, CancelEventArgs e)
+        {
+            textBoxAddPackageFileFullPath.Text = openFileDialogAddPackageFileFullPath.FileName;
+        }
+
+        private void buttonAddPackageSend_Click(object sender, EventArgs e)
+        {
+            var answer = ApiController.CreatePackage(Convert.ToInt32(textBoxAddPackageHandlerId.Text), textBoxAddPackageFileFullPath.Text, textBoxAddPackageInfos.Text);
+            richTextBox1.Text = answer.Content;
+        }
+
+        private void buttonGetPackageSend_Click(object sender, EventArgs e)
+        {
+            var version = 0;
+            int.TryParse(textBoxGetPackageSpecificVersion.Text, out version);
+            ApiController.GetPackage(Convert.ToInt32(textBoxGetPackageHandlerId.Text), version);
         }
     }
 }
